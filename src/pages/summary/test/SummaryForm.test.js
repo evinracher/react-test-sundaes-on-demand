56f5024e8984c74e0b3bb87d81cf2bffe2204399
evinracher <eevinley@gmail.com>
2022-09-02 15:06:35 -0500

    test: checkbox enables button

---
+++ src/pages/summary/test/SummaryForm.test.js
@@ -0,0 +1,33 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SummaryForm from "../SummaryForm";
+
+test("checkbox enables confirm order button", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", { name: /i agree/i });
+  const button = screen.getByRole("button", { name: "Confirm order" });
+  expect(checkbox).not.toBeChecked();
+  userEvent.click(checkbox);
+  expect(button).toBeEnabled();
+  userEvent.click(checkbox);
+  expect(button).toBeDisabled();
+});
+
+test("popover response to hover", () => {
+  render(<SummaryForm />);
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(/no ice cream will actually/i);
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually/i);
+  expect(popover).toBeInTheDocument(); // it is not necessary, but it is a best practice
+
+  // popover disappears when mouse out
+  userEvent.unhover(termsAndConditions);
+  const nullPopoverFinal = screen.queryByText(/no ice cream will actually/i);
+  expect(nullPopoverFinal).not.toBeInTheDocument();
+});
