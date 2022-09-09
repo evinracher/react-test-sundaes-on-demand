e5fb7cd9f1a4f11a77d7da92fbb0b5fc48a8162c
evinracher <eevinley@gmail.com>
2022-09-09 09:59:46 -0500

    trest: toppings options

--- src/pages/entry/tests/Options.test.js
+++ src/pages/entry/tests/Options.test.js
@@ -7,8 +7,26 @@ test("displays image for each scoop option from server", async () => {
 
   // find images, name is alt text
   const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
-  //   expect(scoopImages).toHaveLength()
+  expect(scoopImages).toHaveLength(2);
   // confirm alt text of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images, name is alt text
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+  // confirm alt text of images
+  const altText = toppingImages.map((element) => element.alt);
+
+  expect(altText).toEqual([
+    "M&Ms topping",
+    "Hot fudge topping",
+    "Peanut butter cups topping",
+  ]);
 });
